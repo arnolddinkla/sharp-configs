fd675354526748834c82912ac6cb8511e6a12c8a
Matthew Hoare <matthew.t.hoare@gmail.com>
2020-03-14 09:59:45 +0000

    10.3.2

--- usr/lib/firefox-esr/browser/defaults/preferences/all-sharpbang.js
+++ usr/lib/firefox-esr/browser/defaults/preferences/all-sharpbang.js
@@ -3,3 +3,4 @@ pref("extensions.pocket.enabled", false);
 pref("browser.newtabpage.enabled", false);
 pref("browser.startup.homepage", "about:blank");
 pref("app.normandy.enabled",false);
+pref("beacon.enabled",false);
