8dbd4092940ea1d3474ea93a5f9d351299f1d2c2
Matthew Hoare <matthew.t.hoare@gmail.com>
2020-03-28 10:31:56 +0000

    10.3.3

--- usr/lib/firefox-esr/browser/defaults/preferences/all-sharpbang.js
+++ usr/lib/firefox-esr/browser/defaults/preferences/all-sharpbang.js
@@ -4,3 +4,5 @@ pref("browser.newtabpage.enabled", false);
 pref("browser.startup.homepage", "about:blank");
 pref("app.normandy.enabled",false);
 pref("beacon.enabled",false);
+pref("browser.safebrowsing.enabled",false);
+pref("browser.safebrowsing.malware.enabled",false);
